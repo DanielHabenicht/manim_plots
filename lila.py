f74283de164e31abf578ef3d1b92b936d7f0a992
DanielHabenicht <daniel-habenicht@outlook.de>
2023-06-04 10:39:21 +0000

    update

--- lila.py
+++ lila.py
@@ -9,8 +9,8 @@ config.frame_width = 20
 class GetZAxisLabelExample(ThreeDScene):
     
     def construct(self):
-        # self.camera.background_color=WHITE
-        # self.set_camera_orientation(phi=90 * DEGREES, theta=-90 * DEGREES,  frame_center=(-0.3,0,0), zoom=1.0)
+        # # self.camera.background_color=WHITE
+        # # self.set_camera_orientation(phi=90 * DEGREES, theta=-90 * DEGREES,  frame_center=(-0.3,0,0), zoom=1.0)
 
         # g = Graph([],[], labels=True)
         # g.add_vertices("pump", vertex_type=Rectangle, positions={"pump":(-4,4,0)}, vertex_config={"width": 1, "height": 1, "fill_opacity": 1})
@@ -44,10 +44,14 @@ class GetZAxisLabelExample(ThreeDScene):
         junction1_ax.set_x(-7)
         junction1_ax.set_y(4)
         junction1_ax.set_z(0)
-        labx = junction1_ax.get_x_axis_label(Tex("Time"))
-        laby = junction1_ax.get_y_axis_label(Tex("Pressure"))
+        labx = junction1_ax.get_x_axis_label(Tex("Time").scale(0.7))
+        labx.shift(2*LEFT)
+        labx.shift(0.7*DOWN)
+        laby = junction1_ax.get_y_axis_label(Tex("Flow").scale(0.7))
+        laby.shift(1*LEFT)
+        laby.shift(0.1*DOWN)
         line1 = junction1_ax.plot(lambda t: np.cos(t) + 0.5 * np.cos(9 * t) + (1 / 7) * np.cos(12 * t) + 2, color=BLUE,)
-        junction1_group = VGroup(junction1_ax, line1)
+        junction1_group = VGroup(junction1_ax, line1, labx, laby)
 
 
         junction2_ax = ThreeDAxes(x_range=(0, 4, 1), x_length=2, y_range=(0,4,1), y_length=2, axis_config={
@@ -62,10 +66,14 @@ class GetZAxisLabelExample(ThreeDScene):
         junction2_ax.set_x(7)
         junction2_ax.set_y(0)
         junction2_ax.set_z(0)
-        labx = junction2_ax.get_x_axis_label(Tex("Time"))
-        laby = junction2_ax.get_y_axis_label(Tex("Pressure"))
+        labx = junction2_ax.get_x_axis_label(Tex("Time").scale(0.7))
+        labx.shift(2*LEFT)
+        labx.shift(0.7*DOWN)
+        laby = junction2_ax.get_y_axis_label(Tex("Pressure").scale(0.7))
+        laby.shift(1.2*LEFT)
+        laby.shift(1.6*DOWN)
         line2 = junction2_ax.plot(lambda t: np.cos(t) + 0.5 * np.cos(7 * t) + (1 / 7) * np.cos(14 * t) + 2, color=RED,)
-        junction2_group = VGroup(junction2_ax, line2)
+        junction2_group = VGroup(junction2_ax, line2, labx, laby)
 
 
         junction3_ax = ThreeDAxes(x_range=(0, 4, 1), x_length=2, y_range=(0,4,1), y_length=2, axis_config={
@@ -81,10 +89,14 @@ class GetZAxisLabelExample(ThreeDScene):
         junction3_ax.set_y(-4)
         junction3_ax.set_z(0)
         
-        labx = junction3_ax.get_x_axis_label(Tex("Time"))
-        laby = junction3_ax.get_y_axis_label(Tex("Pressure"))
+        labx = junction3_ax.get_x_axis_label(Tex("Time").scale(0.7))
+        labx.shift(2*LEFT)
+        labx.shift(1.6*DOWN)
+        laby = junction3_ax.get_y_axis_label(Tex("Pressure").scale(0.7))
+        laby.shift(1.2*LEFT)
+        laby.shift(1.6*DOWN)
         line3 = junction3_ax.plot(lambda t: np.cos(t) + 0.5 * np.cos(3 * t) + (1 / 7) * np.cos(20 * t) + 2, color=RED,)
-        junction3_group = VGroup(junction3_ax, line3)
+        junction3_group = VGroup(junction3_ax, line3, labx, laby)
 
 
         self.add(junction3_group, junction1_group, junction2_group)
@@ -93,43 +105,79 @@ class GetZAxisLabelExample(ThreeDScene):
 
         # g.remove_vertices("pump", "junction1", "junction2", "background1", "background2", "background3")
 
-        junction1_group.set_x(0)
-        junction1_group.set_y(0)
+        # junction1_group.set_x(0)
+        # junction1_group.set_y(0)
 
-        junction2_group.set_x(0)
-        junction2_group.set_y(0)
+        # junction2_group.set_x(0)
+        # junction2_group.set_y(0)
 
-        junction3_group.set_x(-1.25)
-        junction3_group.set_y(0)
-        junction3_group.rotate(90, [0,1,0])
-        self.remove(junction2_group)
+        # junction3_group.set_x(0)
+        # junction3_group.set_y(0)
         self.wait()
 
-        dot_1 = Dot3D(point=junction1_ax.coords_to_point(0, 0, 1), color=RED)
-        dot_2 = Dot3D(point=junction1_ax.coords_to_point(2, 0, 0), radius=0.1, color=BLUE)
-        dot_3 = Dot3D(point=[0, 0, 0], radius=0.1, color=ORANGE)
-        self.add(dot_1, dot_2,dot_3)
-
-        self.set_camera_orientation(phi=45 * DEGREES, theta=45 * DEGREES)
-
-        resolution_fa = 10
-
-        def param_gauss(u, v):
-            x = u
-            y = v
-            sigma, mu = 0.4, [0.0, 0.0]
-            d = np.linalg.norm(np.array([x - mu[0], y - mu[1]]))
-            z = np.exp(-(d ** 2 / (2.0 * sigma ** 2)))
-            return np.array([x, y, z])
-
-        gauss_plane = Surface(
-            param_gauss,
-            resolution=(resolution_fa, resolution_fa),
-            v_range=[-1.5, +1.5],
-            u_range=[-1.5, +1.5]
-        )
-        self.add(gauss_plane)
+        text = Text('Bernoulli', font_size=16).scale(3).set_y(5)
+        tex = Tex(r"$P_i=k^0_{ji}+P_{j}k^i_{ji}+\sum_{d\in D_{IR}}k^d_{ji}Q^2_d$", font_size=16).scale(3).set_y(4)
+        self.add(text)
+        self.add(tex)
         self.wait()
+
+
+        main = ThreeDAxes(x_range=(0, 4, 1), 
+                          x_length=3, 
+                          y_range=(0,4,1), 
+                          y_length=3, 
+                          z_range=(0,4,1), 
+                          z_length=3, 
+                          axis_config={
+                "include_ticks": False,
+                # "include_tip": False,
+            },
+            # z_axis_config={
+            #     "include_tip":False,
+            #     "stroke_opacity": 0,
+            #     "fill_opacity": 0
+            # }
+              )
+        main.rotate(45, [1,0,0])
+        labels = main.get_axis_labels(Tex("Pressure - $P_i$").scale(0.7), Tex("Flow - $Q_d$").scale(0.7), Tex("Pressure - $P_j$").scale(0.7))
+        # line3 = junction3_ax.plot(lambda t: np.cos(t) + 0.5 * np.cos(3 * t) + (1 / 7) * np.cos(20 * t) + 2, color=RED,)
+        main_group = VGroup(main, labels)
+
+        self.add(main_group)
+        self.wait()
+
+
+        # cloud = PointCloudDot(color=RED)
+        # self.add(
+        #     cloud
+        # )
+        # self.wait()
+
+        # # dot_1 = Dot3D(point=junction1_ax.coords_to_point(0, 0, 1), color=RED)
+        # # dot_2 = Dot3D(point=junction1_ax.coords_to_point(2, 0, 0), radius=0.1, color=BLUE)
+        # # dot_3 = Dot3D(point=[0, 0, 0], radius=0.1, color=ORANGE)
+        # # self.add(dot_1, dot_2,dot_3)
+
+        # # self.set_camera_orientation(phi=45 * DEGREES, theta=45 * DEGREES)
+
+        # resolution_fa = 10
+
+        # def param_gauss(u, v):
+        #     x = u
+        #     y = v
+        #     sigma, mu = 0.4, [0.0, 0.0]
+        #     d = np.linalg.norm(np.array([x - mu[0], y - mu[1]]))
+        #     z = np.exp(-(d ** 2 / (2.0 * sigma ** 2)))
+        #     return np.array([x, y, z])
+
+        # gauss_plane = Surface(
+        #     param_gauss,
+        #     resolution=(resolution_fa, resolution_fa),
+        #     v_range=[-1.5, +1.5],
+        #     u_range=[-1.5, +1.5]
+        # )
+        # self.add(gauss_plane)
+        # self.wait()
 
 
 
